d2c66515fd9aea3f92b86759dd27c1714505f5e0
pford68 <pford68@earthlink.net>
2024-01-08 08:34:23 -0500

    Adding types

---
+++ src/types/types.ts
@@ -0,0 +1,34 @@
+export type Consumer<T> = (value: T) => void;
+export type BiConsumer<T, U> = (value1:T, value2:U) => void;
+export type Operator<T> = (value:T) => T;
+export type Predicate<T> = (value:T) => boolean;
+export type Supplier<T> = () => T;
+export type Adapter<T, R> = (value:T) => R;
+export type BiFunction<T, U, R> = (value1:T, value2:U) => R;
+export type Struct = {[key:string]: unknown};
+export type Coordinates = {rowIndex: number, colIndex: number};
+export type DataTypes =
+    | "string"
+    | "number"
+    | "currency"
+    | "date"
+    | "datetime"
+    | "boolean"
+    | "object"
+    | "array"
+    | "map"
+    | "enum";
+
+export interface Command<T> {
+    execute(): boolean;
+    undo(): boolean,
+    redo(): boolean,
+    setParameter(param: T): void;
+    getParameters(): T[];
+    icon?: string,
+    name?: string,
+}
+
+
+
+
