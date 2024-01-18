53c2169cdc5734ea2d2f2367cae136f27fa85a48
pford68 <dev@philipford.com>
2024-01-18 14:54:01 -0500

    WIP JAG-35 Implement context menus
    * Renderers were not using the readonly prop. It was not passed to StatefulInput.

--- src/js/renderers/BooleanRenderer.tsx
+++ src/js/renderers/BooleanRenderer.tsx
@@ -18,6 +18,7 @@ export default function BooleanRenderer(props: BooleanRendererProps): React.Reac
         active,
         format,
         validator,
+        readonly,
     } = props;
 
     const baseClassName = joinCss(styles.renderer, styles.boolean, className);
@@ -25,6 +26,7 @@ export default function BooleanRenderer(props: BooleanRendererProps): React.Reac
     const inputProps = {
         name,
         value: (value === true ? "true" : "false"),
+        readonly,
         className: joinCss(baseClassName, styles.active),
         // "switch" is weeded out, but format == switch is handled later on.
         type: format,
